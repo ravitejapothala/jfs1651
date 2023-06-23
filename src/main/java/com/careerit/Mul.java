1fb4d692fad145e5ce5ebc25324d4fb744e87d23
ravitejapothala <121153949+ravitejapothala@users.noreply.github.com>
2023-06-23 11:10:05 -0700

    'second commit'

---
+++ src/main/java/com/careerit/Mul.java
@@ -0,0 +1,11 @@
+package com.careerit;
+
+public class Mul {
+    public static void main(String[] args) {
+        int num1=10;
+
+        for (int i=1;i<=10;i++)
+        {
+        System.out.println(String.format("%s * %s = %s",num1,i,num1*i) );
+    }
+}}
