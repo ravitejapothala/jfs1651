65ba37db887bcdb8a8591631420cec0ffda62f4d
ravitejapothala <121153949+ravitejapothala@users.noreply.github.com>
2023-06-23 10:48:45 -0700

    firrst commit

---
+++ src/main/java/com/careerit/SunofTwoNumbers.java
@@ -0,0 +1,10 @@
+package com.careerit;
+
+public class SunofTwoNumbers {
+    public static void main(String[] args) {
+        int num1=10;
+        int num2=20;
+        System.out.println("sdum f tow nimbers "+ (num1+num2));
+
+    }
+}
---
+++ src/main/java/com/careerit/hellworld.java
@@ -0,0 +1,7 @@
+package com.careerit;
+
+public class hellworld {
+    public static void main(String[] args) {
+        System.out.println("hewllo");
+    }
+}
